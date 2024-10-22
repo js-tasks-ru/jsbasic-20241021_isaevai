f60de05d679348895af013bfa7de79b811904dc6
AlekseyIsaev89 <iaiaykhal7737@gmail.com>
2024-10-22 22:35:16 +0900

    correction 4 tasks, 1-module

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,7 +1,4 @@
 function checkSpam(str) {  // мой код...
   let strtoLowerCase = str.toLowerCase();
-  if (( strtoLowerCase.includes('1xbet') ) || ( strtoLowerCase.includes('xxx') )) { 
-      return true;
-  } 
-  return false;
+  return strtoLowerCase.includes('1xbet')  ||  strtoLowerCase.includes('xxx');
 }
