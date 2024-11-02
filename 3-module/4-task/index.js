dcb55d8c9b1d31445f47eebd5a6397c70b7e1802
AlekseyIsaev89 <iaiaykhal7737@gmail.com>
2024-11-02 22:40:10 +0900

    4, 5 tasks, 3-module

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,9 +1,5 @@
 function showSalary(users, age){
-   let selectedUsers = '';
-   for (let i = 0; i < users.length; i++) {
-      if(users[i].age <= age){
-         selectedUsers += `${users[i].name}, ${users[i].balance}\n`;
-      }
-   }
-   return selectedUsers.substring(0, selectedUsers.length - 1);
+   let filterUsers = users.filter(user => user.age <= age);
+   let result = filterUsers.map(user => `${user.name}, ${user.balance}`)
+   return result.join('\n');      
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,12 +1,10 @@
 function getMinMax(str) {
-   let arr = str.split(' ');
-   arr = arr.filter(item => isFinite(item))
-   
-   let min = Math.min(...arr);
-   let max = Math.max(...arr);
-   
-   return {
-    min,
-    max,
-   }
+  const arr = str
+  .split(' ')
+  .filter(item => Number(item));
+
+  return {
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+  };
 }
