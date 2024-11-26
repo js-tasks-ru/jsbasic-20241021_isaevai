a8fdf0c97e9a9b667ff9c41fb6be2d53d33d95b5
AlekseyIsaev89 <iaiaykhal7737@gmail.com>
2024-11-26 21:37:19 +0900

    6-module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,34 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+
+    const thead = document.createElement('thead');
+    thead.innerHTML = `
+     <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+    `;
+    this.elem.appendChild(thead);
+    const tbody = document.createElement('tbody');
+    this.elem.appendChild(tbody);
+    rows.forEach(row => {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `
+         <td>${row.name}</td>
+            <td>${row.age}</td>
+            <td>${row.salary}</td>
+            <td>${row.city}</td>
+            <td><button>X</button></td>
+      `;
+      tr.querySelector('button').addEventListener('click', () => {
+        tr.remove();
+      });
+      tbody.appendChild(tr);
+    });
   }
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,58 @@
+import createElement from "../../assets/lib/create-element.js";
+
 export default class ProductCard {
   constructor(product) {
+    this.name = product.name;
+    this.price = product.price;
+    this.category = product.category;
+    this.image = product.image;
+    this.id = product.id;
+
+    this.render();
   }
+
+  render = () => {
+    this.elem = createElement(`
+      <div class="card">
+        <div class="card__top">
+          <img src=${this.getPathImage()}
+            class="card__image"
+            alt="product"
+          >
+          <span class="card__price">€${this.getprice()}</span>
+        </div>
+
+      <div class="card__body">
+        <div class="card__title">${this.name}</div>
+
+        <button type="button" class="card__button">
+          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+        </button>
+      </div>
+    </div>
+  `);
+
+    this.elem.addEventListener("click", this.add);
+  };
+
+  add = (e) => {
+    const isButton = e.target.closest(".card__button");
+
+    if (isButton) {
+      const eventProductAdd = new CustomEvent("product-add", {
+        detail: this.id,
+        bubbles: true,
+      });
+
+      this.elem.dispatchEvent(eventProductAdd);
+    }
+  };
+
+  getprice = () => {
+    return this.price.toFixed(2);
+  };
+
+  getPathImage = () => {
+    return `/assets/images/products/${this.image}`;
+  };
 }
